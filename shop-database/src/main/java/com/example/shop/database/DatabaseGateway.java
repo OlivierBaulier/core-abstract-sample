66641e22a4b7662d491ed77d7e56df1e0fce9a91
OlivierBaulier <olivier.baulier@oseline.com>
2022-07-17 19:41:07 +0200

    Refactoring model with 2 tables

--- shop-database/src/main/java/com/example/shop/database/DatabaseGateway.java
+++ shop-database/src/main/java/com/example/shop/database/DatabaseGateway.java
@@ -14,7 +14,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
-
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -22,12 +21,27 @@ import java.util.stream.IntStream;
 @Component
 public class DatabaseGateway implements DatabaseAdapter {
 
-    private static final String FILTERED_STOCK_SQL = "SELECT color, size, count(stock_id) AS quantity FROM SHOES_STOCK WHERE  ( :color IS  NULL OR color = :color ) AND (:size IS  NULL OR size =:size ) GROUP BY color, size";
-    private static final String CATALOG_SQL = "SELECT DISTINCT 'Shop shoe' as name, color, size FROM SHOES_STOCK  WHERE ( :color IS  NULL OR color = :color ) AND (:size IS  NULL OR size =:size )";
-    private static final String STOCK_SQL = "SELECT color, size, count(case outputDate when IS NULL then 1 else null end ) AS quantity FROM SHOES_STOCK  GROUP BY color, size";
-    private static final String INSERT_SQL = "INSERT INTO SHOES_STOCK (color, size) VALUES(:color, :size)";
-    private static final String FILTERED_COUNT_SQL = "SELECT count(stock_id) AS quantity FROM SHOES_STOCK WHERE outputDate IS  NULL AND ( :color IS  NULL OR color = :color ) AND (:size IS  NULL OR size =:size )";
-    private static final String DESTOCK_SQL = "UPDATE SHOES_STOCK set outputDate= NOW() WHERE outputDate IS NULL AND color = :color AND SIZE = :size LIMIT :quantity";
+
+    private static final String INSERT_SQL = "INSERT INTO SHOES_STOCK(model_id) (SELECT model_id FROM shoes_model  WHERE name = :name AND color = :color AND SIZE = :size)";
+    private static final String STOCK_SQL = "SELECT name, color, size, count(S.stock_id) AS quantity FROM SHOES_MODEL M LEFT JOIN shoes_stock S ON S.model_id = M.model_id AND S.outputDate IS NULL "+
+            " GROUP BY M.name, M.color, M.size";
+    private static final String FILTERED_STOCK_SQL ="SELECT name, color, size, count(S.stock_id) AS quantity FROM SHOES_MODEL M LEFT JOIN shoes_stock S ON S.model_id = M.model_id AND S.outputDate IS NULL " +
+            "WHERE S.outputDate IS NULL  AND ( :color IS  NULL OR M.color = :color ) AND (:size IS  NULL OR M.size =:size ) AND (:name IS  NULL OR M.name =:name ) " +
+            "GROUP BY M.name, M.color, M.SIZE";
+
+    private static final String DESTOCK_SQL = "UPDATE SHOES_STOCK set outputDate= NOW() WHERE outputDate IS NULL AND model_id IN ( SELECT model_id FROM SHOES_MODEL M " +
+            "WHERE M.color = :color AND SIZE = :size) LIMIT :quantity";
+
+    private static final String CATALOG_SQL = "SELECT name, color, size FROM SHOES_MODEL  WHERE ( :color IS  NULL OR color = :color ) AND (:size IS  NULL OR size =:size )";
+
+    private static final String FILTERED_COUNT_SQL ="SELECT count(stock_id) AS quantity FROM SHOES_MODEL M LEFT JOIN shoes_stock S ON S.model_id = M.model_id AND S.outputDate IS NULL " +
+            "WHERE  ( :color IS  NULL OR M.color = :color ) AND (:size IS  NULL OR M.size =:size ) AND (:name IS  NULL OR M.name =:name ) ";
+
+    private static final String IS_MODEL_EXIST_SQL = "SELECT count(model_id) FROM shoes_model  WHERE name = :name AND color = :color AND SIZE = :size;  ";
+
+    private static final String ADD_MODEL = "INSERT INTO SHOES_MODEL (name, color, size) VALUES(:name,:color, :size);";
+
+
 
     @Autowired
     @Qualifier("myJdbcTemplate")
@@ -37,10 +51,6 @@ public class DatabaseGateway implements DatabaseAdapter {
     @Qualifier("myNamedParameterJdbcTemplate")
     private NamedParameterJdbcTemplate myNamedParameterJdbcTemplate;
 
-
-    public List<AvailableShoe> getAllShoes() {
-        return myJdbcTemplate.query(STOCK_SQL, new AvailableShoeMapper() );
-    }
 
     @Override
     public List<Shoe> getCatalog(ShoeFilter filter) {
@@ -59,7 +69,8 @@ public class DatabaseGateway implements DatabaseAdapter {
     public List<AvailableShoe> getStockWithFilter(FilterEntity filter) {
         SqlParameterSource parameters = new MapSqlParameterSource()
                 .addValue("color", filter.getColor())
-                .addValue("size", filter.getSize());
+                .addValue("size", filter.getSize())
+                .addValue("name", "Shop shoe");
         return myNamedParameterJdbcTemplate.query(FILTERED_STOCK_SQL, parameters,  new AvailableShoeMapper());
     }
 
@@ -67,17 +78,20 @@ public class DatabaseGateway implements DatabaseAdapter {
     public int countShoes(FilterEntity filter) {
         SqlParameterSource parameters = new MapSqlParameterSource()
                 .addValue("color", filter.getColor())
-                .addValue("size", filter.getSize());
-        return myNamedParameterJdbcTemplate.queryForObject(FILTERED_COUNT_SQL, parameters, Integer.class).intValue();
+                .addValue("size", filter.getSize())
+                .addValue("name", "Shop shoe");
+        Integer result = myNamedParameterJdbcTemplate.queryForObject(FILTERED_COUNT_SQL, parameters, Integer.class);
+        return (result!= null)? result: 0;
     }
 
     @Override
     public int stock(StockMovement movement) {
-
+        this.addModelIfNotExist(movement);
         return IntStream.range(0, movement.getQuantity()).map(i -> {
             SqlParameterSource parameters = new MapSqlParameterSource()
                     .addValue("color", movement.getColor())
-                    .addValue("size", movement.getSize());
+                    .addValue("size", movement.getSize())
+                    .addValue("name", "Shop shoe");
             return myNamedParameterJdbcTemplate.update(INSERT_SQL, parameters);
         }).sum();
 
@@ -90,6 +104,20 @@ public class DatabaseGateway implements DatabaseAdapter {
                 .addValue("size", movement.getSize())
                 .addValue("quantity", - movement.getQuantity());
         return - myNamedParameterJdbcTemplate.update(DESTOCK_SQL, parameters);
+    }
+
+    private void addModelIfNotExist(StockMovement movement){
+        SqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("color", movement.getColor())
+                .addValue("size", movement.getSize())
+                .addValue("name", "Shop shoe");
+        Integer isExist = myNamedParameterJdbcTemplate.queryForObject(IS_MODEL_EXIST_SQL, parameters, Integer.class);
+        if(isExist != null && isExist ==1) return;
+        parameters = new MapSqlParameterSource()
+                .addValue("color", movement.getColor())
+                .addValue("size", movement.getSize())
+                .addValue("name", "Shop shoe");
+         myNamedParameterJdbcTemplate.update(ADD_MODEL, parameters);
     }
 
 }
